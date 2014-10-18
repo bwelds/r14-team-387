aa66f973ec4dd061380c05eecf87d01ace4ac9ca
Bianca Welds <bwelds@gmail.com>
2014-10-17 21:15:11 -0500

    Added basic authentication to hide site

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == 'bianca' and password == 'whatthehell'
+end
+
 get '/' do
   erb :"index", layout: :"layouts/main"
 end
