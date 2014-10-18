54afb96712c2f46379a00f439bfe6bc7daaa66e2
Bianca Welds <bwelds@gmail.com>
2014-10-18 15:03:56 -0500

    Added connection to Twitter and display user's timeline as test

--- main.rb
+++ main.rb
@@ -137,18 +137,59 @@ post '/index' do
 
 end
 
+get '/milestones' do
+	@tweets = @client.home_timeline
+	erb :"milestones", layout: :"layouts/main"
+end
+
 get '/about' do
 	erb :"about", layout: :"layouts/main"
 
 end
 
+# store the request tokens and send to Twitter
 get '/connect' do
-	request_token = @client.request_token(
-		
+	request_token = @client.authentication_request_token(
+		:oauth_callback => ENV['CALLBACK_URL']
 		)
 	session[:request_token] = request_token.token
 	session[:request_token_secret] = request_token.secret
-	redirect request_token.authorize_url  
+	redirect request_token.authorize_url.gsub('authorize', 'authenticate')  
+end
+
+# auth URL is called by twitter after the user has accepted the application
+# this is configured on the Twitter application settings page
+get '/auth' do
+  # Exchange the request token for an access token.
+  
+  begin
+    @access_token = @client.authorize(
+      session[:request_token],
+      session[:request_token_secret],
+      :oauth_verifier => params[:oauth_verifier]
+    )
+  rescue OAuth::Unauthorized
+  end
+  
+  if @client.authorized?
+      # Storing the access tokens so we don't have to go back to Twitter again
+      # in this session.  In a larger app you would probably persist these details somewhere.
+      session[:access_token] = @access_token.token
+      session[:secret_token] = @access_token.secret
+      session[:user] = true
+      redirect '/milestones'
+    else
+      redirect '/connect'
+  end
+end
+
+get '/disconnect' do
+  session[:user] = nil
+  session[:request_token] = nil
+  session[:request_token_secret] = nil
+  session[:access_token] = nil
+  session[:secret_token] = nil
+  redirect '/'
 end
 
 get '/ping' do 
