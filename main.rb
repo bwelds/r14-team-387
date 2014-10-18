498e2be731b0164ced631c3910f61d622f362a84
Bianca Welds <bwelds@gmail.com>
2014-10-18 00:15:47 -0500

    Modify average display to not show 0 results

--- main.rb
+++ main.rb
@@ -16,6 +16,11 @@ helpers do
   			number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1#{delimiter}").reverse
 		end
         
+        def average_per_time(recent,period)
+
+        	recent/period
+
+        end
 
 end
 
@@ -29,11 +34,19 @@ end
 post '/index' do
 	   @handle = params[:handle]
 	   @total = rand(15000)
+	   @averages = "and makes on average "
 	   
 	   @next_1k = next_highest(@total,1000)
 	   @next_5k = next_highest(@total,5000)
 	   @next_10k = next_highest(@total,10000)
 
+	   @average_per_hour = average_per_time(@total/100,672)
+	   @average_per_day = average_per_time(@total/100,28)
+	   @average_per_week = average_per_time(@total/100,4)
+
+	   if @average_per_hour >= 1 then @averages = @averages + @average_per_hour.to_s+" tweets per hour, " end
+	   if @average_per_day >= 1 then @averages = @averages + @average_per_day.to_s+" tweets per day, " end
+	   if @average_per_week >= 1 then @averages =  @averages + " and "+@average_per_week.to_s+" tweets over an entire week." end
 
 
 	   erb :"content", layout: :"layouts/main"
